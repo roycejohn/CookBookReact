a92dfa44d84b5826594e29e4e5d2d08a4b3ce7e3
roycejohn <cuevasroycejohn.com>
2024-05-29 12:19:03 +0200

    gh pages setup

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,14 +1,28 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import {BrowserRouter} from 'react-router-dom';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter basename="/CookBookReact">
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+//   </BrowserRouter>
+  
+// )
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/CookBookReact">
+  <Router basename="/CookBookReact">
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>
+  </Router>
   
 )
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,10 +6,21 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: '/CookBookReact/',
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/CookBookReact/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist'
+  }
+});
